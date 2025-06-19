0c2fb30c25f91d606cae064ac065c95ab03f35e3
Man Guy <makay.mate1011@gmail.com>
2025-06-19 01:26:53 +0100

    changes

--- script.js
+++ script.js
@@ -4,8 +4,4 @@ const urlDisplay = document.querySelector('.url-display');
 
 copyButton.addEventListener('click', () => {
     navigator.clipboard.writeText(urlDisplay.textContent);
-    copyButton.textContent = 'Copied!';
-    setTimeout(() => {
-        copyButton.textContent = 'Copy';
-    }, 2000);
-});
+})
